d933cb269e7dec6f931fa5fe8177de8cd9d5fa60
Praneeth Ganesh <gpraneeth20@gmail.com>
2025-04-17 21:40:50 +0530

    new changes

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.onscroll=()=>{
 }
 
 const typed = new Typed('.multiple-text', {
-    strings: ['DevOps Engineer ', 'Blockchain Developer','Frontend Developer','Backend developer'],
+    strings: ['DevOps Engineer', 'Blockchain Developer','Frontend Developer','Backend developer'],
     typeSpeed: 60,
     backSpeed: 60,
     backDelay: 1000,
